40adb764a0435b55e77ee751e07ef17375d7ce79
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-07 00:28:51 +0600

    final push after added photo api

--- photos.js
+++ photos.js
@@ -5,7 +5,26 @@
 function loadData (){
     fetch('https://jsonplaceholder.typicode.com/photos')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayData(data))
+}
+
+// display data
+function displayData (photos){
+    const displaySection = document.getElementById('photos-section');
+
+    for(const photo of photos){
+        console.log(photo);
+        const div = document.createElement('div');
+        div.classList.add('color-me');
+        div.innerHTML = `
+        <img src="${photo.thumbnailUrl}" >
+        <h4>Title: ${photo.title}</h4>
+        <p>Id: ${photo.id}</p>
+        <p>Album Id: ${photo.albumId}</p>
+        <a href="${photo.url}"><button id="color-me-btn">Click Me</button></a>
+        `;
+        displaySection.appendChild(div);
+    }
 }
 
 loadData();
